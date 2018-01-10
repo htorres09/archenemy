dfa90996176c23fff3fdfa0f172dee4f70efd31a
htorres09 <lcc.h.torres09@gmail.com>
2018-01-10 17:33:40 -0600

    Adjusted attributes
    Atributos ajustados

--- script.js
+++ script.js
@@ -5,8 +5,8 @@
 document.querySelector('.btn-scheme').addEventListener('click', function(){
     var x = document.createElement("IMG");
     x.setAttribute("src", "img.jpg");
-    x.setAttribute("width", "300");
-    x.setAttribute("height", "450");
+    x.setAttribute("width", "100%");
+    x.setAttribute("height", "100%");
     x.setAttribute("Alt", "On motion: Something");
     document.getElementById("target").appendChild(x);
 });
