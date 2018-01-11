c49f979ed97583ff2fef119dad429d4ff6ca2586
htorres09 <lcc.h.torres09@gmail.com>
2018-01-10 21:07:39 -0600

    Layout & Images
    CSS layout mod
    Scheme Images

--- script.js
+++ script.js
@@ -3,14 +3,27 @@
  */
 
 document.querySelector('.btn-scheme').addEventListener('click', function(){
-    var x = document.createElement("IMG");
-    x.setAttribute("src", "img.jpg");
-    x.setAttribute("width", "100%");
-    x.setAttribute("height", "100%");
-    x.setAttribute("Alt", "On motion: Something");
-    document.getElementById("target").appendChild(x);
+  //var y = document.getElementById("target");
+  //y.parentElement.removeChild(y);
+
+  var x = document.createElement("IMG");
+  x.setAttribute("src", "imgs/img.jpg");
+  x.setAttribute("width", "100%");
+  x.setAttribute("height", "100%");
+  x.setAttribute("Alt", "On motion: Something");
+  document.getElementById("target").appendChild(x);
 });
 
- function nextScheme(){
-     //ToDO Something
+function rmvScheme(){
+  var x = document.getElementById("target");
+  x.parentElement.removeChild(x);
+}
+
+function nxtScheme(){
+   var x = document.createElement("IMG");
+   x.setAttribute("src", "imgs/img.jpg");
+   x.setAttribute("width", "100%");
+   x.setAttribute("height", "100%");
+   x.setAttribute("Alt", "On motion: Something");
+   document.getElementById("target").appendChild(x);
  }
